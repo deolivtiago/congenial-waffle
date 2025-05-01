66b4557ba82469e18593bb99eaaa8bfbe5ca79c1
Tiago de Oliveira <deolivtiago@outlook.com>
2025-05-01 16:43:31 -0300

    chore: init spring web app

---
+++ src/main/java/com/clarxlabs/ellion/EllionApplication.java
@@ -0,0 +1,13 @@
+package com.clarxlabs.ellion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EllionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EllionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarxlabs/ellion/EllionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarxlabs.ellion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EllionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
